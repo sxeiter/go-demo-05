1b29fddceb49cc1b291bc597a3a863cbac221523
Pavel Baksheev <sxeiter@mail.ru>
2025-08-26 18:07:39 +0500

    начало проекта

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Новый проект")
+	city := flag.String("city", "", "city пользователя")
+	format := flag.Int("format", 1, "Формат вывода погоды")
+	flag.Parse()
+	fmt.Println(*city)
+	fmt.Println(*format)
 }
